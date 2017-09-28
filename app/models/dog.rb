28f03c591f82fb1ccac93c35fe037c111d73df41
Sarah Schaller <sarcsch@aol.com>
2017-09-28 13:58:58 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,10 @@
 class Dog
-  attr_accessor :name, :breed, :age 
+  attr_accessor :name, :breed, :age
 
   def initialize(name, breed, age)
-    @name = name 
-    @breed = breed 
-    @age = age 
+    @name = name
+    @breed = breed
+    @age = age
   end
   
 end
